ef17568128224759a101e74e177a9f76968ee836
LUCAS FELIPE MARCELINO DA SILVA <lucas.fmsilva10@senacsp.edu.br>
2024-05-10 21:23:21 -0300

    my-app-dev

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import {styles} from './styles'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Escolha {"\n"}
+      <Text style={styles.subtitle}>os produtos</Text>
+      </Text>
+      <Text style={styles.message}> 
+        Descubra receitas baseads nos produtos que você escolheu 
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
---
+++ styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      padding:50
+    //   alignItems: 'center',
+    //   justifyContent: 'center',
+    },
+    title:{
+      fontSize: 36,
+      fontFamily: 'bold',
+      lineHeight:44,
+      marginTop: 42,
+    }, 
+    subtitle:{
+    }, 
+    message:{
+      fontSize:20,
+      color:'#7C78A',
+      marginTop:12,
+      marginBottom: 38
+    }
+  });
